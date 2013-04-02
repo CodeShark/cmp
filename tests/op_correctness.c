a58ce33858893c28edc2539fd642a7a47ceb89c6
Eric Lombrozo <elombrozo@gmail.com>
2013-04-02 04:57:01 -0700

    Added multiple iterations to op_correctness test.

--- tests/op_correctness.c
+++ tests/op_correctness.c
@@ -16,28 +16,31 @@ int main()
 
     cmp_srand(12345);
 
-    //cmp_uint64_set_hex(a, 4, "ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff");
-    //cmp_uint64_set_hex(b, 4, "ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff");
-    cmp_uint64_rand(a, 4);
-    cmp_uint64_rand(b, 4);
-    cmp_uint64_get_hex(ahex, HEX_SIZE, a, 4);
-    cmp_uint64_get_hex(bhex, HEX_SIZE, b, 4);
+    int i = 0;
+    for (; i < 10; i++) {
+        cmp_uint64_rand(a, 4);
+        cmp_uint64_rand(b, 4);
+        cmp_uint64_get_hex(ahex, HEX_SIZE, a, 4);
+        cmp_uint64_get_hex(bhex, HEX_SIZE, b, 4);
 
-    cmp_uint64_add(r, a, b, 4, 1);
-    cmp_uint64_get_hex(rhex, HEX_SIZE, r, 5);
-    printf("%s + %s = %s\n", ahex, bhex, rhex);
+        cmp_uint64_add(r, a, b, 4, 1);
+        cmp_uint64_get_hex(rhex, HEX_SIZE, r, 5);
+        printf("%s + %s = %s\n", ahex, bhex, rhex);
 
-    int sign = cmp_uint64_sub(r, a, b, 4);
-    cmp_uint64_get_hex(rhex, HEX_SIZE, r, 4);
-    printf("%s - %s = %s with sign = %d\n", ahex, bhex, rhex, sign);
+        int sign = cmp_uint64_sub(r, a, b, 4);
+        cmp_uint64_get_hex(rhex, HEX_SIZE, r, 4);
+        printf("%s - %s = %s with sign = %d\n", ahex, bhex, rhex, sign);
 
-    cmp_uint64_add_word(r, a, 4, 1, 1);
-    cmp_uint64_get_hex(rhex, HEX_SIZE, r, 5);
-    printf("%s + 1 = %s\n", ahex, rhex);
+        cmp_uint64_add_word(r, a, 4, 1, 1);
+        cmp_uint64_get_hex(rhex, HEX_SIZE, r, 5);
+        printf("%s + 1 = %s\n", ahex, rhex);
 
-    cmp_uint64_mul_4(r, a, b);
-    cmp_uint64_get_hex(rhex, HEX_SIZE, r, 8);
-    printf("%s * %s = %s\n", ahex, bhex, rhex);
+        cmp_uint64_mul_4(r, a, b);
+        cmp_uint64_get_hex(rhex, HEX_SIZE, r, 8);
+        printf("%s * %s = %s\n", ahex, bhex, rhex);
+
+        printf("-------------------------\n");
+    }
 
     return 0;
 }
